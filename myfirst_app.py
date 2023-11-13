b3ad0211a019226cd7c1e5f03c0ae30664706c57
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-13 07:40:03 +0530

    Update myfirst_app.py
    add Emojis to the menu items

--- myfirst_app.py
+++ myfirst_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('Oh ! My God Its My New First streamlit Message')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and BluBerry Oatmeal')
-streamlit.text('Kale & Spinach Rocket meal')
-streamlit.text('Hard Boiled Egg')
+streamlit.text('🥣 Omega 3 and BluBerry Oatmeal')
+streamlit.text('🥗 Kale & Spinach Rocket meal')
+streamlit.text('🐔 Hard Boiled Egg')
+streamlit.text('🥑🍞 Brandy Toast')
 
