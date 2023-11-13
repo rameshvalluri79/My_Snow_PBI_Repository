8adf247ea8ec3bd0e6bc893eb8b4412ab6f88bf6
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-13 07:37:22 +0530

    Update myfirst_app.py
    updated test to text

--- myfirst_app.py
+++ myfirst_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('Oh ! My God Its My New First streamlit Message')
 streamlit.header('Breakfast Menu')
-streamlit.test('Omega 3 and BluBerry Oatmeal')
-streamlit.test('Kale & Spinach Rocket meal')
-streamlit.test('Hard Boiled Egg')
+streamlit.text('Omega 3 and BluBerry Oatmeal')
+streamlit.text('Kale & Spinach Rocket meal')
+streamlit.text('Hard Boiled Egg')
 
