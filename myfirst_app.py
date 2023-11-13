be551cd2aef9e3a0114ec461bcb4068fa56e165c
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-13 07:36:02 +0530

    Update myfirst_app.py
    Menu Added

--- myfirst_app.py
+++ myfirst_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('Oh ! My God Its My New First streamlit Message')
+streamlit.header('Breakfast Menu')
+streamlit.test('Omega 3 and BluBerry Oatmeal')
+streamlit.test('Kale & Spinach Rocket meal')
+streamlit.test('Hard Boiled Egg')
+
